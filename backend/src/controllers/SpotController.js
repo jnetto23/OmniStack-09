3d67cab5ea80e2555a4697dccf27c12f2a5079d9
Joao Netto <jnetto@fyyb.com.br>
2020-02-09 23:41:26 -0300

    created backend - application of week OmniStack 09

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const Spot = require("../models/Spot");
+
+module.exports = {
+  async index(req, res) {
+    const { tech } = req.query;
+
+    const spots = await Spot.find({ techs: tech });
+
+    return res.json(spots);
+  },
+
+  async store(req, res) {
+    const { filename } = req.file;
+    const { company, price, techs } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: "User does not exists" });
+    }
+
+    let spot = await Spot.findOne({ company });
+
+    if (!spot) {
+      spot = await Spot.create({
+        user,
+        thumbnail: filename,
+        company,
+        techs: techs.split(",").map(tech => tech.trim()),
+        price
+      });
+    }
+    return res.json(spot);
+  }
+};
